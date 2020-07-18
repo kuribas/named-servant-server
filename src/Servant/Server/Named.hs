a4d25e442a68f91b5a4dc8dacc357195e3bcf0fb
Kristof Bastiaensen 3e <kba@3e.eu>
2020-07-18 15:37:25 +0200

    add support for records

--- src/Servant/Server/Named.hs
+++ src/Servant/Server/Named.hs
@@ -8,9 +8,10 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 -- | This module just exports orphan instances to make named-servant
 -- work with servers
-module Servant.Named.Server () where
+module Servant.Server.Named () where
 import Servant.API
 import Servant.Server
 import Servant.Named
@@ -42,4 +43,17 @@ instance (KnownSymbol sym, FromHttpApiData a, HasServer api context)
 
   route Proxy context subserver =
     route (Proxy :: Proxy (QueryParams sym a :> api)) context $
+    fmap (\f x -> f (Arg x)) subserver
+
+instance (KnownSymbol sym, HasServer api context)
+      => HasServer (NamedQueryFlag sym :> api) context where
+
+  type ServerT (NamedQueryFlag sym :> api) m =
+    sym :! Bool -> ServerT api m
+
+  hoistServerWithContext _ pc nt s =
+    hoistServerWithContext (Proxy :: Proxy api) pc nt . s
+
+  route Proxy context subserver =
+    route (Proxy :: Proxy (QueryFlag sym :> api)) context $
     fmap (\f x -> f (Arg x)) subserver
